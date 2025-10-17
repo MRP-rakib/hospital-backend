fee63bc7bba6ae5a2f16bcc25d16837ab82baac9
MRP-rakib <rakib260.hossain@gmail.com>
2025-10-17 21:07:53 +0600

    signup route create

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,14 +1,28 @@
-const {createUser} = require('../services/signupUser')
+const { createUser } = require("../services/signupUser");
+const validator = require("validator");
+const User = require("../models/userSchema");
+const signupController = async (req, res, next) => {
+  try {
+    const { firstname, lastname, email, password } = req.body;
+// fields checking
+    if (!firstname || !lastname || !email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+// email checking
+    if (!validator.isEmail(email))
+      return res.status(400).json({ message: "invalid email format" });
+// existinguser checking
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "email already registered" });
+// user create
+    const newUser = await createUser({ firstname, lastname, email, password });
+    res.status(201).json({ message: "Account resgister succesfull" });
+  } catch (error) {
+    error.status = 400;
+    console.log(error);
 
-const signupController=async(req,res,next)=>{
-    try {
-       const newUser = await createUser(req.body)
-       
-       res.status(201).json({message:'account resgister succesfull'})
-      } catch (error) {
-         error.status = 400
-        next(error)
-    }
-}
+    next(error);
+  }
+};
 
-module.exports =signupController
+module.exports = signupController;
