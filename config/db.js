d8c807cf4fb25309c799ea9b570ebef727997806
MRP-rakib <rakib260.hossain@gmail.com>
2025-10-21 22:57:23 +0600

    change on database connect

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGOOSE}`);
+    await mongoose.connect(process.env.MONGOOSE,{
+      useNewUrlParser:true,
+      useUnifiedTopology: true
+    });
     console.log("mongoose connected");
   } catch (error) {
     console.log(error);
